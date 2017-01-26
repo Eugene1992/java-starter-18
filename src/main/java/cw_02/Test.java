025088abfeecd6ba409c18873c32ac90514c0686
Eugene1992 <deyneko55@gmail.com>
2017-01-26 18:46:12 +0200

    Added hw02.

--- src/main/java/cw_02/Test.java
+++ src/main/java/cw_02/Test.java
@@ -2,8 +2,25 @@ package cw_02;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
+
+        boolean flag1 = true;  //  Ctrl + /
+        boolean flag2 = false; /*  Ctrl + Shift + / */
+
+        char symb1 = 'h';
+        char symb2 = 104;
+        char symb3 = '\u2343'; // Unicode
+
+        System.out.println(symb2);
+
+        byte b = 100; // 0000 0000
+        short s = 32000;
+        int i = 2_000_000_000;
+        long l = 2_000_000_000_000_000_000L;
+
+        float f = 100.0F;
+        double d1 = 100.0;
+        double d2 = 100;
+
+        System.out.println(d2);
     }
 }
---
+++ src/main/java/cw_02/TestOperations.java
@@ -0,0 +1,23 @@
+package cw_02;
+
+public class TestOperations {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a / b);
+        System.out.println(a * b);
+
+        System.out.println(8 % 3);
+
+        System.out.println("Hello world!");
+        System.out.print("Hello world!");
+        System.out.println("Hello world!");
+
+        a = a + b;
+
+        System.out.println(a);
+    }
+}
