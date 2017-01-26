5393b491afa82fc739b9bc1fa55f2522febc9e92
DarKnighttt <vovchik2008@gmail.com>
2017-01-26 17:39:12 +0200

    My first commit.

--- vladimir/src/main/java/Test.java
+++ vladimir/src/main/java/Test.java
@@ -1,2 +1,8 @@
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("My first commit");
+        int a=10;
+        a +=213;
+        System.out.println("A = " + a);
+    }
 }
