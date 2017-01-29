25c98e9de4d90cf8f067a929a3c567159963615c
grytsayo <grytsayo@gmail.com>
2017-01-29 11:10:25 +0200

    Test commit.

---
+++ oleg/src/main/java/hw/hw02/Test3.java
@@ -0,0 +1,11 @@
+package hw.hw02;
+
+
+public class Test3
+{
+    public static void main(String[] args)
+    {
+        short b = 140-39;
+        System.out.print (b);
+    }
+}
---
+++ oleg/src/main/java/hw/hw02/Test4.java
@@ -0,0 +1,9 @@
+package hw.hw02;
+public class Test4
+{
+    public static void main(String [] args)
+    {
+        int b = 17836/675;
+        System.out.print (b);
+    }
+}
