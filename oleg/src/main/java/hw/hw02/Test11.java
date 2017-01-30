a1179389c73bb776f16c5b009e0fe5762cb315e0
grytsayo <grytsayo@gmail.com>
2017-01-30 22:31:26 +0200

    Test commit.

--- oleg/src/main/java/hw/hw02/Test11.java
+++ oleg/src/main/java/hw/hw02/Test11.java
@@ -2,18 +2,32 @@ package hw.hw02;
 
 public class Test11 {
     public static void main(String[] args) {
-        int a = 50;
-        int b = (a - (a / 2));
-        b = b / 2;
-        int b1 = (b - (b / 2));
-        b1 = b1 / 2;
-        int b2 = (b1 - (b1 / 2));
-        b2 = b2 / 2;
-        int b3 = (b2 - (b2 / 2));
+        int a = 26;
+        int b51 = a % 2;
+        int f = a / 2;
+        int b41 = f % 2;
+        int f1 = f / 2;
+        int b31 = f1 % 2;
+        int f2 = f1 / 2;
+        int b21 = f2 % 2;
+        int f3 = f2 / 2;
+        int b11 = f3 % 2;
+        int f4 = f3 / 2;
+        int b10 = f4 % 2;
+        int f5 = f4 / 2;
+        int b9 = f5 % 2;
+        int f6 = f5 / 2;
+        int b8 = f6 % 2;
 
-        System.out.print(b3);
-        System.out.print(b2);
-        System.out.print(b1);
-        System.out.print(b);
+            System.out.print(b8);
+            System.out.print(b9);
+            System.out.print(b10);
+            System.out.print(b11);
+        System.out.print(" ");
+        System.out.print(b21);
+        System.out.print(b31);
+        System.out.print(b41);
+        System.out.print(b51);
+
     }
 }
