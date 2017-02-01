61ce25fe737f185988f48831020d7d561e06dbc9
DarKnighttt <vovchik2008@gmail.com>
2017-02-01 17:55:16 +0200

    Binarv2.0 class work add.

--- vladimir/src/main/java/cw04/Binar.java
+++ vladimir/src/main/java/cw04/Binar.java
@@ -6,10 +6,12 @@ public class Binar {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt(), b;
+        String c = "";
         while (a != 1) {
             b = a % 2;
-            System.out.println(b);
+            c = b + c;
             a /= 2;
         }
+        System.out.println(c);
     }
 }
