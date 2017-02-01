2eaa102b6e1d2c4e79362ae88a48675244b21067
DarKnighttt <vovchik2008@gmail.com>
2017-02-01 17:31:05 +0200

    Completed hw3.

---
+++ vladimir/src/main/java/cw04/Sum.java
@@ -0,0 +1,34 @@
+package cw04;
+
+
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a, b, a1, a2, b1, b2;
+        System.out.println("Enter number a");
+        a = scan.nextInt();
+        System.out.println("Enter number b");
+        b = scan.nextInt();
+        a1 = a;
+        a2 = a;
+        b1 = b;
+        b2 = b;
+        while (a <= b) {
+            a += a;
+            a++;
+        }
+        System.out.println(a);
+        do {
+            a1 += a1;
+            a1++;
+        } while (a1 <= b1);
+        System.out.println(a1);
+
+        for (; a2 <= b2; a2++) {
+            a2 += a2;
+        }
+        System.out.println(a2);
+    }
+}
