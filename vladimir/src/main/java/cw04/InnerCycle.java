cf02b262c1cba127b6769adf15efd400b24b96ac
DarKnighttt <vovchik2008@gmail.com>
2017-02-01 18:22:05 +0200

    InnerCycle class work add.

---
+++ vladimir/src/main/java/cw04/InnerCycle.java
@@ -0,0 +1,13 @@
+package cw04;
+
+public class InnerCycle {
+    public static void main(String[] args) {
+        for (int i =0; i < 10; i++){
+            System.out.println("*");
+            for (int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+        }
+        System.out.println("*");
+    }
+}
