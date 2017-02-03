c1dea9788cb500fbb3709bd805c3b6c77fbe1a50
DarKnighttt <vovchik2008@gmail.com>
2017-02-03 18:33:18 +0200

    Game cw5 added. + number of atemps

--- vladimir/src/main/java/Game_cw05.java
+++ vladimir/src/main/java/Game_cw05.java
@@ -5,6 +5,7 @@ public class Game_cw05 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String exit;
+        int i = 0;
         do {
             System.out.println("Input number");
             exit = scan.next();
@@ -13,9 +14,16 @@ public class Game_cw05 {
                     int n = Integer.parseInt(exit);
                     Random rand = new Random();
                     int r = rand.nextInt(10);
-                    if (n == r)
+                    if (n == r) {
                         System.out.println("You win!");
-                    else System.out.println("You wrong.");
+                        i++;
+                        System.out.println("Number of atemps " + i);
+                    }
+                    else{
+                        System.out.println("You wrong.");
+                        i++;
+                        System.out.println("Number of atemps " + i);
+                    }
                     System.out.println("Try again?\ny - yes\nn - no");
                     exit = scan.next();
                 } else System.out.println("Wrong input");
