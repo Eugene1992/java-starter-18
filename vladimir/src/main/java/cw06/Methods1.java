6cde8f5fa74434b311a6c4173703fa4758ea85c3
DarKnighttt <vovchik2008@gmail.com>
2017-02-07 17:35:37 +0200

    Method1 cw06 added.

---
+++ vladimir/src/main/java/cw06/Methods1.java
@@ -0,0 +1,21 @@
+package cw06;
+
+
+import java.util.Scanner;
+
+public class Methods1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number of strings");
+        int n = scan.nextInt();
+        printStr(n);
+    }
+
+
+    static void printStr(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("Hello world number = " + i);
+
+        }
+    }
+}
