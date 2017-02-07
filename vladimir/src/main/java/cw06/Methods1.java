1945771f82a37b752f423aadb263286501ce21e1
DarKnighttt <vovchik2008@gmail.com>
2017-02-07 17:41:41 +0200

    Method1.v2 cw06 added.

--- vladimir/src/main/java/cw06/Methods1.java
+++ vladimir/src/main/java/cw06/Methods1.java
@@ -6,15 +6,15 @@ import java.util.Scanner;
 public class Methods1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter number of strings");
-        int n = scan.nextInt();
-        printStr(n);
+        System.out.println("Enter string");
+        String str = scan.nextLine();
+        printStr(str);
     }
 
 
-    static void printStr(int n) {
-        for (int i = 1; i <= n; i++) {
-            System.out.println("Hello world number = " + i);
+    static void printStr(String str) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.printf("%s number = %d\n",str,i);
 
         }
     }
