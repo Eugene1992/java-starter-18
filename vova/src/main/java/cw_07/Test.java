cbf1386fc9ca2f838a7a668e856c4991e7ea01f5
VladimirShuliak <123kpi123>
2017-02-10 13:31:23 +0200

    cw_07

--- vova/src/main/java/cw_07/Test.java
+++ vova/src/main/java/cw_07/Test.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        my5Array(array);
+        my6Array(array);
     }
 
     static void my2Array(int[] array) {
@@ -23,22 +23,30 @@ public class Test {
         }
 
     }
-    static void my3Array(int [] array){
-        for(int i = array.length - 1; i >= 0; i--){
+
+    static void my3Array(int[] array) {
+        for (int i = array.length - 1; i >= 0; i--) {
             System.out.print(array[i] + " ");
         }
     }
-    static void my4Array(int [] array){
-        for(int i = 0; i < array.length/2; i++ ){
+
+    static void my4Array(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
             System.out.print(array[i] + " ");
         }
     }
-    static void my5Array(int [] array){
-        for(int i = array.length/2; i < array.length; i++){
+
+    static void my5Array(int[] array) {
+        for (int i = array.length / 2; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
     }
-    static void my6Array(int [] array){
-//        for(int i)
+
+    static void my6Array(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        System.out.print(sum);
     }
 }
